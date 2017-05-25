9fbf84512b97e73d0b5032e145f75ae94d2c1620
ErwindeWolff <erwin_de_wolff@live.nl>
2017-05-25 12:11:52 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from variables import *
 
 
-#var = PredictionNode(names = ["Coin Outcome", "Fairness", "Brightness", "Person"],values=["Heads", "Tails"] ,
-#                 parentValues =[["Fair", "Unfair"], ["Dark", "Light"], ["Wouter", "Erwin"]]) 
+def printVar(var):
+    print var.names[0] + " given " + str(var.names[1::]) + "\n"
+    (foo, bar) = var.getProbabilityTable()
+    for i in range(len(foo)):
+        print foo[i], bar[i]
+    print ""
 
-var = HyperpriorVariable(names = ["Coin Outcome", "Fairness"],values=["Heads", "Tails"] ,
-                 parentValues =[["Fair", "Unfair"]]) 
-
-var2 = Variable(names = ["Fairness"], values=["Fair", "Unfair"], parentValues=[])
-
-print var.names[0] + " given " + str(var.names[1::]) + "\n"
-(foo, bar) = var.getProbabilityTable()
-for i in range(len(foo)):
-    print foo[i], bar[i]
-print ""
+var = HyperpriorVariable(names = ["Coin Outcome", "Fairness", "Brightness", "Person"],values=["Heads", "Tails"] ,
+                 parentValues =[["Fair", "Unfair"], ["Dark", "Light"], ["Wouter", "Erwin"]]) 
 
 
-print var2.names[0] + " given " + str(var2.names[1::]) + "\n"
-(foo, bar) = var2.getProbabilityTable()
-for i in range(len(foo)):
-    print foo[i], bar[i]
-print ""
+
+#var = HyperpriorVariable(names = ["Coin Outcome", "Fairness"],values=["Heads", "Tails"] ,
+#                 parentValues =[["Fair", "Unfair"]]) 
+
+#var2 = Variable(names = ["Fairness"], values=["Fair", "Unfair"], parentValues=[])
+
+printVar(var)
+#printVar(var2)
+
