bc28a8728a73ceea40d574216b8bf4d8d4dc8638
ErwindeWolff <erwin_de_wolff@live.nl>
2017-06-01 11:33:36 +0200

    Updated planing and made framework for experiment

---
+++ predProc.py
@@ -0,0 +1,26 @@
+from math import log
+
+# Returns kullback-leiber divergence between prediction and observed
+# Where in the definition, P = obs ad Q = pred
+def KLD (obs, pred):
+    return sum([x1 * log((x1/x2), 2) for x1, x2 in zip(obs, pred) if x1 != 0.0])
+
+
+def simulation( agent_model, world_model, nr_iterations, sampling=False, weighting = False):
+
+    hypo_nodes = agent_model.getHypthesisNodes()    
+    pred_nodes = agent_model.getPredictionNodes()
+
+
+    pred_errors = list() 
+
+    for _ in range(nr_iterations):
+
+        hypo_truth = world_model.observe(hypo_nodes)
+
+        pred =  agent_model.getPrediction(pred_nodes, hypo_truth, sampling)
+        obs = world_model.getPrediction(pred_nodes, hypo_truth, sampling)
+
+        pred_error = KLD(obs, pred)
+
+        agent_model.update(obs, pred_error, weighting)
